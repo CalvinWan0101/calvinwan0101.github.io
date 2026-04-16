61b5efe42b860f2ee45a1de926707a1121939068
CalvinWan0101 <calvinwan0101@gmail.com>
2026-04-16 22:42:54 +0800

    [Feature] Add new purchase entry.

--- src/apps/Portfolio/Portfolio.tsx
+++ src/apps/Portfolio/Portfolio.tsx
@@ -132,6 +132,7 @@ const purchaseHistory: PurchaseGroup[] = [
   {
     month: '2026 年 4 月',
     entries: [
+      { date: '2026/04/15', shares: '1.33730', price: '$148.059644' },
       { date: '2026/04/06', shares: '1.40090', price: '$139.910013' },
     ],
   },

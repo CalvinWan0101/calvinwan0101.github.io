6cafbda577b5e2e1b499e16dcde53ed9cb5ca7a0
CalvinWan0101 <calvinwan0101@gmail.com>
2026-04-22 00:24:44 +0800

    [Refactor] Restructure Layout component to use styled-components. 1. Wrapped the main layout in a styled div (LayoutShell) for better styling control. 2. Removed fragment shorthand and replaced it with the styled component.

--- src/apps/layout/Layout.tsx
+++ src/apps/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 import { AnalyticsTracker } from './analytics'
 import { BackgroundAudio } from './audio'
 import { Footer } from './footer'
@@ -6,12 +7,18 @@ import { Navbar } from './navbar'
 
 export const Layout = () => {
   return (
-    <>
+    <LayoutShell>
       <Navbar />
       <Outlet />
       <Footer />
       <AnalyticsTracker />
       <BackgroundAudio />
-    </>
+    </LayoutShell>
   )
 }
+
+const LayoutShell = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
